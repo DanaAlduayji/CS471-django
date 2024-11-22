c7032e13117c98599484ab8753ba934f2d660336
Dana <danab1422@gmail.com>
2024-11-23 02:04:38 +0300

    Initial commit

---
+++ web_lab_5_6-master/apps/bookmodule/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views  # Import views from the current directory
+
+
+urlpatterns = [
+    path('', views.index),
+    path('index2/', views.index2),  # This will handle the case when val1 is missing
+    path('index2/<int:val1>/', views.index2),  # This will handle the case when val1 is present
+    path('<int:bookId>', views.viewbook),
+    path('html5/links/', views.link_page, name='link_page'),
+    path('html5/text/formatting', views.formatting_view, name='formatting'),
+    path('html5/listing', views.listing_view, name='listing'),
+    path('html5/tables', views.tables_view, name='tables'),
+    path('', views.index, name= "books.index"), 
+    path('list_books/', views.list_books, name= "books.list_books"), 
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
+    path('aboutus/', views.aboutus, name="books.aboutus"), 
+    path('search/', views.search_books, name='search_books'),
+
+]
+
